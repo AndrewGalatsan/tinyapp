44dcb906af2e66cda330e104a48cda3feaa03e5d
AndrewGalatsan <agalatsan@hotmail.com>
2022-12-31 19:17:18 +0000

    Store key value pair and redirect urls to id

--- express_server.js
+++ express_server.js
@@ -1,4 +1,3 @@
-function generateRandomString() {}
 
 const express = require("express");
 const app = express();
@@ -15,8 +14,22 @@ const urlDatabase = {
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateShortURL();
+  const newURL = req.body.longURL;
+  urlDatabase[shortURL] = newURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+// redirect to longRIL
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  if (verifyShortUrl(shortURL)) {
+    const longURL = urlDatabase[shortURL];
+    res.redirect(longURL);
+  } else {
+    res.status(404);
+    res.send('Does not exist');
+  }
 });
 
 app.get("/", (req, res) => {
@@ -41,20 +54,44 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[req.params.shortURL];
-  const templateVars = { id: req.params.id, longURL: longURL};
-  res.render("urls_show", templateVars);
+  let shortURL = req.params.id;
+  if (verifyShortUrl(shortURL)) {
+    let longURL = urlDatabase[req.params.shortURL];
+    let templateVars = { shortURL: shortURL, longURL: longURL };
+    res.render("urls_show", templateVars);
+  } else {
+    res.send('does not exist');
+  }
 });
-
-
-
-
-
-
-
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = () => {
+  const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  const upperCase = lowerCase.toUpperCase();
+  const numeric = '1234567890';
+  const alphaNumeric = lowerCase + upperCase + numeric;
+  //alphaNumeric is 62
+  let index = Math.round(Math.random() * 100);
+  if (index > 61) {
+    while (index > 61) {
+      index = Math.round(Math.random() * 100);
+    }
+  }
+  return alphaNumeric[index];
+};
+
+const generateShortURL = () => {
+  let randomString = '';
+  while (randomString.length < 6) {
+    randomString += generateRandomString();
+  }
+  return randomString;
+};
+
+const verifyShortUrl = URL => {
+  return urlDatabase[URL];
+};
