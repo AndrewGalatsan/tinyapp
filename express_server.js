dd4cf663f34c88caf6b200372b726b52787da4d7
AndrewGalatsan <agalatsan@hotmail.com>
2022-12-31 18:26:40 +0000

    Added new post function

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+function generateRandomString() {}
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -9,6 +11,13 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,6 +36,9 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   let shortURL = req.params.shortURL;
